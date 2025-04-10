aea69a4d80c166752230a9b3d37e210605d884ca
HarshaBuddha <buddhaharsha12@gmail.com>
2025-04-10 22:08:39 +0530

    Updating port for deployment

--- app/Flask/app.py
+++ app/Flask/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import joblib
 import numpy as np
 import pandas as pd
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -40,4 +41,5 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)

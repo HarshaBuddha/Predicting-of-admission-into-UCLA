135bc01ab9c0f2b6fc9015491a6ef1b083dae8e7
HarshaBuddha <buddhaharsha12@gmail.com>
2025-04-12 12:21:09 +0530

    Vercel deployment changes and adding timeout

--- app/ucla-admission-frontend/src/components/predictorform.jsx
+++ app/ucla-admission-frontend/src/components/predictorform.jsx
@@ -40,7 +40,7 @@ const Predictor = () => {
       const formattedData = Object.fromEntries(
         Object.entries(formData).map(([key, value]) => [key, parseFloat(value)])
       );
-      const response = await axios.post(`${API_URL}/predict`, formattedData);
+      const response = await axios.post(`${API_URL}/predict`, formattedData,{timeout:50000});
       setPrediction(response.data.admission_chance);
       toast.success("Prediction received!");
     } catch (error) {
